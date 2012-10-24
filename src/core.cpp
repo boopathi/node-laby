e5298385f9a527ff4783a9ce997820f9f49d17ca
Boopathi Rajaa <me@boopathi.in>
2012-10-24 18:24:55 +0530

    Build is now becomming successful

--- src/core.cpp
+++ src/core.cpp
@@ -66,7 +66,7 @@ namespace orm {
 			//default db init functions
 			try {
 				driver = get_driver_instance();
-				con = driver->connect(data.);
+				//con = driver->connect();
 			} catch( SQLException &e) {
 				
 			}
